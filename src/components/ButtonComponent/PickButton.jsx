9f1c75e420f41e2bd7688c75f88f09aed0304ac4
ysh9698 <75485765+ysh9698@users.noreply.github.com>
2024-03-08 15:06:17 +0900

    main 마지막

--- src/components/ButtonComponent/PickButton.jsx
+++ src/components/ButtonComponent/PickButton.jsx
@@ -4,70 +4,70 @@ import { IoAddOutline } from "react-icons/io5";
 import { IoMdCheckmark } from "react-icons/io";
 
 const CommonStyle = css`
-    width: ${(props) => (props.btnSize ? props.btnSize : "2rem")};
-    height: ${(props) => (props.btnSize ? props.btnSize : "2rem")};
-    font-size: ${(props) => (props.fontSize ? props.fontSize : "2rem")};
-    padding: ${(props) => (props.paddingSize ? props.paddingSize : "5px")};
-    border-radius: 50%;
-    border: 2px solid
-        ${(props) => (props.colorCode ? props.colorCode : "white")};
-    cursor: pointer;
-    color: ${(props) => (props.colorCode ? props.colorCode : "white")};
+  width: ${(props) => (props.btnSize ? props.btnSize : "2rem")};
+  height: ${(props) => (props.btnSize ? props.btnSize : "2rem")};
+  font-size: ${(props) => (props.fontSize ? props.fontSize : "2rem")};
+  padding: ${(props) => (props.paddingSize ? props.paddingSize : "5px")};
+  border-radius: 50%;
+  border: 2px solid ${(props) => (props.colorCode ? props.colorCode : "white")};
+  cursor: pointer;
+  color: ${(props) => (props.colorCode ? props.colorCode : "white")};
+  background-color: ${(props) =>
+    props.colorCode ? props.colorCode : "transparent"};
+  &:hover {
     background-color: ${(props) =>
-        props.colorCode ? props.colorCode : "transparent"};
-    &:hover {
-        background-color: ${(props) =>
-            props.hoverBackColor ? props.hoverBackColor : "#444444"};
-        color: ${(props) => (props.hoverColor ? props.hoverColor : "white")};
-    }
-    &:active {
-        background-color: ${(props) =>
-            props.hoverBackColor ? props.hoverBackColor : "#bababa"};
-        color: ${(props) => (props.hoverColor ? props.hoverColor : "white")};
-    }
+      props.hoverBackColor ? props.hoverBackColor : "#444444"};
+    color: ${(props) => (props.hoverColor ? props.hoverColor : "white")};
+  }
+  &:active {
+    background-color: ${(props) =>
+      props.hoverBackColor ? props.hoverBackColor : "#bababa"};
+    color: ${(props) => (props.hoverColor ? props.hoverColor : "white")};
+  }
+  margin-right: 10px;
 `;
 
 const AddBtnStyle = styled(IoAddOutline)`
-    ${CommonStyle}
+  ${CommonStyle}
 `;
 
 const PickedBtnStyle = styled(IoMdCheckmark)`
-    ${CommonStyle}
+  ${CommonStyle}
 `;
 
 function PickButton({
-    btnSize,
-    fontSize,
-    paddingSize,
-    colorCode,
-    hoverBackColor,
-    hoverColor,
-    isPicked,
-    setIsPicked,
+  btnSize,
+  fontSize,
+  paddingSize,
+  colorCode,
+  hoverBackColor,
+  hoverColor,
+  isPicked,
+  setIsPicked,
 }) {
-    return (
-        <div onClick={() => setIsPicked((prev) => !prev)}>
-            {isPicked ? (
-                <AddBtnStyle
-                    btnSize={btnSize}
-                    fontSize={fontSize}
-                    paddingSize={paddingSize}
-                    colorCode={colorCode}
-                    hoverBackColor={hoverBackColor}
-                    hoverColor={hoverColor}
-                ></AddBtnStyle>
-            ) : (
-                <PickedBtnStyle
-                    btnSize={btnSize}
-                    fontSize={fontSize}
-                    paddingSize={paddingSize}
-                    colorCode={colorCode}
-                    hoverBackColor={hoverBackColor}
-                    hoverColor={hoverColor}
-                ></PickedBtnStyle>
-            )}
-        </div>
-    );
+  return (
+    <div onClick={() => setIsPicked((prev) => !prev)}>
+      {isPicked ? (
+        <AddBtnStyle
+          btnSize={btnSize}
+          fontSize={fontSize}
+          paddingSize={paddingSize}
+          colorCode={colorCode}
+          hoverBackColor={hoverBackColor}
+          hoverColor={hoverColor}
+        ></AddBtnStyle>
+      ) : (
+        <PickedBtnStyle
+          btnSize={btnSize}
+          fontSize={fontSize}
+          paddingSize={paddingSize}
+          colorCode={colorCode}
+          hoverBackColor={hoverBackColor}
+          hoverColor={hoverColor}
+        ></PickedBtnStyle>
+      )}
+    </div>
+  );
 }
 
 export default PickButton;
